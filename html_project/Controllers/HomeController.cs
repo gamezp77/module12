95042ff57d60bee412f55c6c2e86317d2ab2f313
Pablo Gamez Jr <>
2021-03-23 10:20:47 -0600

    Module 12

---
+++ html_project/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using html_project.Models;
+
+namespace html_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult SaveUser(string UserName, string Password)
+        {
+            string myResults =
+                "The user is:" + UserName +
+                " and the Passoword is:" + Password;
+            return Content(myResults);
+        }
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
